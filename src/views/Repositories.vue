3ff08ad5b663b7dcf9d40d430c923c40740fb206
mariabelinska <mariabelinskaa@gmail.com>
2020-03-04 09:55:11 +0200

    add search by repository name

--- src/views/Repositories.vue
+++ src/views/Repositories.vue
@@ -1,10 +1,20 @@
 <template>
   <v-container>
-    <h1>Repositories</h1>
-    <pulse-loader v-if="repositories.length === 0" color="#673ab7"></pulse-loader>
+    <h1 class="mx-3">Repositories</h1>
+    <v-text-field
+      class="mx-3 my-3"
+      flat
+      rounded
+      hide-details
+      label="repository name"
+      prepend-inner-icon="search"
+      filled
+      v-model="searchQuery"
+    ></v-text-field>
+    <pulse-loader v-if="repositories.length === 0" color="#673ab7" class="my-6 mx-3"></pulse-loader>
     <RepositoryCard
       v-else
-      v-for="repository in repositories"
+      v-for="repository in filteredRepositories"
       :key="repository.id"
       :repository="repository"
     />
@@ -24,7 +34,19 @@ export default {
   data() {
     return {
       repositories: [],
+      searchQuery: null,
     };
+  },
+  computed: {
+    filteredRepositories() {
+      if (this.searchQuery) {
+        return this.repositories.filter(item => {
+          return item.name.startsWith(this.searchQuery);
+        });
+      } else {
+        return this.repositories;
+      }
+    },
   },
   async created() {
     this.repositories = await RepositoryService.getRepositories();
